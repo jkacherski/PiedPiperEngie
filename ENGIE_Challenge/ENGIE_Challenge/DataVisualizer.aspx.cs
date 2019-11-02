fae4fd8104ac2157dc57b7e341648bf8aaf1c3b6
Johnathan Kacherski <kacherski.2@buckeyemail.osu.edu>
2019-11-02 19:46:55 -0400

    doev

--- ENGIE_Challenge/ENGIE_Challenge/DataVisualizer.aspx.cs
+++ ENGIE_Challenge/ENGIE_Challenge/DataVisualizer.aspx.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Data;
+using System.Configuration;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
 
 namespace ENGIE_Challenge
 {
@@ -11,12 +15,51 @@ namespace ENGIE_Challenge
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            // on page load
         }
 
         protected void ddlTheme_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // changing ddl index
+        }
 
+        private void ImportCSVToTable(DataTable dt, string file_path)
+        {
+            string[] csvRows = System.IO.File.ReadAllLines(file_path);
+            string[] fields = null;
+            string[] fieldstemp = Regex.Split(csvRows[0], ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+            int i = 0;
+
+            foreach (string col in fieldstemp)
+            {
+                dt.Columns.Add(col.ToString());
+            }
+
+            foreach (string csvRow in csvRows)
+            {
+                fields = Regex.Split(csvRow, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                DataRow row = dt.NewRow();
+                row.ItemArray = fields;
+                dt.Rows.Add(row);
+            }
+        }
+
+        private void CreateDonutChart()
+        {
+            try
+            {
+                int[] Data;
+                string[] Labels;
+                string[] BackgroundColors;
+                // string[] HubList;
+                // DataTable ChartData = new DataTable();
+                /// NOTE: Run ImportCSVToTable on page load; use global table var for chart data
+ 
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
